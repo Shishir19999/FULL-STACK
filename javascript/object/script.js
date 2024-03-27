51d787dfe36af6a3f2491a4369389c3ac95fc742
Shishir19999 <shishirsharma199@gmail.com>
2024-03-27 14:45:34 +0545

    object

---
+++ javascript/object/script.js
@@ -0,0 +1,50 @@
+let students = {
+  name: 'ram',
+  age: 34,
+  info: {
+    phone: 4578578674,
+    address: 'ktm',
+  },
+};
+
+console.log(students.name);
+console.log(students.age);
+console.log(students.info.address);
+
+let student = [
+  { name: 'ram', age: 34, phone: 34536667,gender:"male" },
+  { name: 'sita', age: 23, phone: 35436363 ,gender:"female"},
+  { name: 'hari', age: 12, phone: 453636773,gender:"male" },
+  { name: 'gita', age: 56, phone: 35253534646,gender:"female" },
+];
+
+console.log(student[0].name);
+console.log(student[1].age);
+
+for (let x = 0; x < student.length; x++) {
+  console.log(student[x].name);
+  console.log(student[x].age);
+  console.log(student[x].phone);
+  console.log(student[x].gender);
+}
+
+student.forEach((s) => {
+  console.log(s.name);
+  console.log(s.age);
+  console.log(s.phone);
+  console.log(s.gender);
+});
+
+let output = '';
+student.forEach((s, index) => {
+  output += `<tr>
+    <td>${++index}</td>
+    <td>${s.name}</td>
+    <td>${s.age}</td>
+    <td>${s.phone}</td>
+    <td>${s.gender}</td>
+    </tr>`;
+});
+
+console.log(output);
+const std=document.getElementById("students-list").innerHTML=output;
