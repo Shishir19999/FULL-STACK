7ad97036d7e1c9b244131841f327faf79a89879b
Shishir19999 <shishirsharma199@gmail.com>
2024-03-28 06:40:12 +0545

    clock

--- javascript/clock/script.js
+++ javascript/clock/script.js
@@ -1,23 +1,22 @@
 console.log(new Date());
 
-let dt=new Date();
-let ld=dt.toLocaleDateString();
-let lt=dt.toLocaleTimeString();
+let dt = new Date();
+let ld = dt.toLocaleDateString();
+let lt = dt.toLocaleTimeString();
 console.log(lt);
 console.log(ld);
 
+setInterval(function () {
+  let dt = new Date();
+  let lt = dt.toLocaleTimeString();
+  console.log(lt);
+  document.getElementById('container').innerHTML = lt;
+}, 1000);
 
-setInterval(function(){
-    let dt=new Date();
-    let lt=dt.toLocaleTimeString();
-    console.log(lt);
-    document.getElementById("container").innerHTML=lt;
-},1000)
-
-function myclock(){
-    let dt=new Date();
-    let lt=dt.toLocaleTimeString();
-    document.getElementById("container").innerHTML=lt;
+function myclock() {
+  let dt = new Date();
+  let lt = dt.toLocaleTimeString();
+  document.getElementById('container').innerHTML = lt;
 }
 
-setInterval(myclock,1000);
+setInterval(myclock, 1000);
--- javascript/slideshow/script.js
+++ javascript/slideshow/script.js
@@ -1,17 +1,17 @@
-let slideindex=0;
+let slideindex = 0;
 
-function customimageslider(){
-    let box=document.getElementsByClassName("sliderbox");
-    for(let i=0;i<box.length;i++){
-        box[i].style.display="none";
-    }
-    slideindex++;
-    if(slideindex>box.length){
-        slideindex=1;
-    }
-    box[slideindex-1].style.display="block";
-    console.log(slideindex);
-    setTimeout(customimageslider,2000);
+function customimageslider() {
+  let box = document.getElementsByClassName('sliderbox');
+  for (let i = 0; i < box.length; i++) {
+    box[i].style.display = 'none';
+  }
+  slideindex++;
+  if (slideindex > box.length) {
+    slideindex = 1;
+  }
+  box[slideindex - 1].style.display = 'block';
+  console.log(slideindex);
+  setTimeout(customimageslider, 2000);
 }
 
 customimageslider();
