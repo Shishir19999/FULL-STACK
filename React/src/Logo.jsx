38f6ec2b0416e5e5f4333377dd556617c40c449e
Shishir19999 <shishirsharma199@gmail.com>
2024-04-02 08:32:05 +0545

    multiple component app logo header and footer

---
+++ React/src/Logo.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Logo() {
+  return (
+    <div>
+      <h1>Logo Section</h1>
+    </div>
+  )
+}
+
--- React/src/main.jsx
+++ React/src/main.jsx
@@ -1,26 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
 
+function Apps() {
+  return (
+    <>
+      <h1>hello world from main.jsx</h1>
+    </>
+  );
+}
 
+let root = document.getElementById('root');
+let h1 = document.createElement("h1");
+h1.textContent = "h1 Tags";
+root.appendChild(h1);
 
-  function Apps(){
-    return(
-      <>
-      <h1>hello world from main jsx</h1>
-      </>
-    )
-  }
-
-
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(root).render(
   <>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  <Apps/>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+    <br />
+    <Apps />
   </>
-  )
-  
+);
