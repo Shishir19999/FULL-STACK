e569ae4742049795932ae595db2d9af2fb646613
Shishir19999 <shishirsharma199@gmail.com>
2024-04-10 12:13:29 +0545

    express and mongodb starts school database added

---
+++ Express/index.js
@@ -0,0 +1,14 @@
+const express =require('express')
+const app=express()
+const port=3000
+
+app.get('/',(req,res)=>{
+    res.send('Hello World')
+})
+app.get('/news',(req,res)=>{
+    res.send('Hello news')
+})
+
+app.listen(port,()=>{
+    console.log("app is listening")
+})
