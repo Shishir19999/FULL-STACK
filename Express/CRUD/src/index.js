7f5c6489f8005a27a659c2f0a87e3213f0eb5944
Shishir19999 <shishirsharma199@gmail.com>
2024-04-15 21:58:04 +0545

    new Project file

---
+++ Express/CRUD/src/index.js
@@ -0,0 +1,71 @@
+const express=require("express");
+
+const mongoose=require('mongoose')
+mongoose.connect('mongodb://localhost:27017/CRUD')
+
+const app=express();
+const studentSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required: true
+    },
+    email:{
+        type:String,
+        required : true,
+        unique: true
+    },
+    phone:{
+        type:String,
+        required:true
+    },
+    address:{
+        type:String
+    }
+
+})
+const students=mongoose.model("students",studentSchema)
+app.use(express.json())
+
+
+app.get('/',async(req,res)=>{
+    let studentsData= await  students.find({})
+    res.status(200).json(studentsData)
+
+});
+
+app.post('/',(req,res)=>{
+    try{
+    const sObj=new students({...req.body});
+    sObj.save()
+    return res.status(201).json({message:"student added Successful"});
+    }
+    catch{
+        res.status(400).json({message:err.message});
+    }
+})
+
+app.put("/:id", async (req , res )=> {
+    let id= req.params.id;
+    try{
+        let student =await students.findByIdAndUpdate(id , req.body);
+        res.status(200).json({student:student,message: "data updated successfully"});
+    }
+    catch(e){
+        res.status(400).json({message : e.message});
+    }
+})
+
+app.delete("/:id", async (req , res )=> {
+    let id= req.params.id;
+    try{
+        await students.findByIdAndDelete(id);
+        res.status(200).json({message: "data deleted successfully"});
+    }
+    catch(e){
+        res.status(400).json({message : e.message});
+    }
+})
+
+app.listen(3000,()=>{
+    console.log("app is lidtening");
+})
