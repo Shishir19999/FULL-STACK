1326ef3415a753a1e48148a4f7cf4459bf8d87a4
Shishir19999 <shishirsharma199@gmail.com>
2024-04-18 15:07:59 +0545

    mern file deleted

--- MERN/backend/src/routes/categoryRouter.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const categoryRouter = express.Router();
-
-categoryRouter.get('/', (req, res) => {
-    res.send('Category Router');
-});
-
-export default categoryRouter;
