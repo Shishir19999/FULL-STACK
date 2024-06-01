a0ba1e2874308e58ae46d006015dc204045f9c97
Shishir19999 <shishirsharma199@gmail.com>
2024-06-01 13:38:11 +0545

    update midlleware cchapte

--- Middlewares/server.js
+++ Middlewares/server.js
@@ -4,19 +4,33 @@ const app =express()
 
 app.use(logger)
 app.get ('/',(req,res)=>{
-
-    res.send("homme page")
+res.send("homme page")
+console.log("hommme.page")
 })
 
-app.get ('/users',(req,res)=>{
-    console.log("user.page")
+app.get ('/users',auth ,(req,res)=>{
+    console.log(`user is admmmin =${req.admin}`)
+    console.log("user page")
     res.send("users page")
 })
+
 function logger (req, res, next ){
-   console.log("log")
-   next() 
+    console.log("before nexxt")
+    console.log(req.originalUrl)
+    next() 
+    console.log("after nexxt")
 }
 
+function auth (req, res, next ){
+   if(req.query.admin ==='true'){
+    req.admin=true
+    next()
+    return
+
+   }
+    res.send('no auth')
+   
+}
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
   });
